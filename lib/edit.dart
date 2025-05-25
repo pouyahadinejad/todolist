54270ae7a52b4464a2f4275260c107f278a8453f
pouyahadinejad <pouya.hadi1382@gmail.com>
2025-05-25 12:54:17 +0330

    add class My widget

--- lib/edit.dart
+++ lib/edit.dart
@@ -185,3 +185,14 @@ class PriortyCheckBox extends StatelessWidget {
     );
   }
 }
+
+
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
